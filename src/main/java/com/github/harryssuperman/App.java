c152856e8b9f03ae55418d97fc6699473738a70c
harryssuperman <harryssuperman@hotmail.com>
2022-06-06 23:20:17 +0200

    Gridpanes

---
+++ src/main/java/com/github/harryssuperman/App.java
@@ -0,0 +1,10 @@
+package com.github.harryssuperman;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
