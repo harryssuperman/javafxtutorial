7b5d2625d1327c02c5680bc17744e1eefd21600d
harryssuperman <harryssuperman@hotmail.com>
2022-10-30 02:48:51 +0200

    ADD: MVVM and ProgressBarApp

---
+++ src/main/java/com/github/harryssuperman/mvvm/EmploymentRequestConverter.java
@@ -0,0 +1,12 @@
+package com.github.harryssuperman.mvvm;
+
+public class EmploymentRequestConverter {
+
+    public EmploymentRequest toEmploymentRequest(EmploymentRequestViewModel viewModel) {
+        return new EmploymentRequest(
+                viewModel.getName(),
+                viewModel.getPosition(),
+                viewModel.getAnnualSalary()
+        );
+    }
+}
---
+++ src/main/java/com/github/harryssuperman/mvvm/EmploymentRequestModel.java
@@ -0,0 +1,8 @@
+package com.github.harryssuperman.mvvm;
+
+public class EmploymentRequestModel {
+
+    public void save(EmploymentRequest req) {
+        System.out.println("saving " + req);
+    }
+}
