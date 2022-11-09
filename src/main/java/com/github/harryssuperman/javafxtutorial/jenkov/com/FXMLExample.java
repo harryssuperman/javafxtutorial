3f244bccbaf5f1f621784ad1a91b2dfd11a63288
harryssuperman <harryssuperman@hotmail.com>
2022-11-09 21:48:31 +0100

    ADD: FXML Example

---
+++ src/main/java/com/github/harryssuperman/javafxtutorial/jenkov/com/FXMLExample.java
@@ -0,0 +1,25 @@
+package com.github.harryssuperman.javafxtutorial.jenkov.com;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class FXMLExample extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(getClass().getClassLoader().getResource("fxml-example.fxml"));
+        VBox vbox = loader.<VBox>load();
+
+        Scene scene = new Scene(vbox);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+}
